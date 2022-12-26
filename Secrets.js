d46e2333fa08ac431f79d10537931ee5a007e353
Kenneth DeBacker <kcdebacker@gmail.com>
2022-12-26 17:14:11 -0500

    Delete Secrets.js

--- Secrets.js
+++
@@ -1,8 +0,0 @@
-export  const firebaseConfig = {
-    apiKey: "AIzaSyAVmKhNDnJekbkdBGc199ZqeFASTa_eFV0",
-    authDomain: "ken-homework-5.firebaseapp.com",
-    projectId: "ken-homework-5",
-    storageBucket: "ken-homework-5.appspot.com",
-    messagingSenderId: "490842897373",
-    appId: "1:490842897373:web:d2e27210dc8ff4268ed434"
-  };
